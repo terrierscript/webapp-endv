1f75974962dd289ad3f5467cafbf5d4b5849d722
terrierscript <13282103+terrierscript@users.noreply.github.com>
2021-04-07 13:50:19 +0900

    add

--- src/pages/api/tree/[word].ts
+++ src/pages/api/tree/[word].ts
@@ -1,16 +1,16 @@
 
-import { NextApiHandler } from "next"
-import { searchSynset, searchLemma } from "../../../lib/dics"
-import { generateTree } from "../../../lib/tree"
+// import { NextApiHandler } from "next"
+// import { searchSynset, searchLemma } from "../../../lib/dics"
+// import { generateTree } from "../../../lib/tree"
 
-const handler: NextApiHandler = async (req, res) => {
-  const { word } = req.query
-  if (typeof word !== "string") {
-    res.status(400).end()
-    return 
-  }
-  const tree = generateTree(word)
-  res.json(tree)
-}
+// const handler: NextApiHandler = async (req, res) => {
+//   const { word } = req.query
+//   if (typeof word !== "string") {
+//     res.status(400).end()
+//     return 
+//   }
+//   const tree = generateTree(word)
+//   res.json(tree)
+// }
 
-export default handler
+// export default handler
--- test/tree.ts
+++
@@ -1,9 +0,0 @@
-import { generateTree } from "../src/lib/tree"
-
-const start = () => {
-
-  console.log("start")
-  console.log(JSON.stringify(generateTree("intuition"), null,2))
-}
-
-start()
