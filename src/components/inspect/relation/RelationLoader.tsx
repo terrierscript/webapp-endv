997dbc18551adb7d708b28e6f2c709ee4104a00f
terrierscript <13282103+terrierscript@users.noreply.github.com>
2021-04-12 21:50:00 +0900

    uw

--- src/components/inspect/relation/RelationLoader.tsx
+++ src/components/inspect/relation/RelationLoader.tsx
@@ -1,7 +1,7 @@
 import React from "react"
 import { Sense, RelationRecord } from "../../../lib/dictionary/types"
 import { RelationAccordion } from "./RelationAccordion"
-import { useWordNet } from "../useWordNet"
+import { useWordNetQuery } from "../useWordNet"
 import { FC } from "react"
 import { ItemAccordion } from "../../ItemAcordion"
 
@@ -9,7 +9,7 @@ import { ItemAccordion } from "../../ItemAcordion"
 export const LoadSenseRelation = ({ sense }: { sense: Sense }) => {
   // console.log(sense)
   // const { senseRelation, synsetRelation } = useSynsetGroupedRelation(sense)
-  const relations = useWordNet<RelationRecord[]>("senseRelation", [sense?.id])
+  const relations = useWordNetQuery<RelationRecord[]>("senseRelation", [sense?.id])
   const rel = relations?.[sense.id]
   if (!rel) {
     return null
@@ -18,7 +18,7 @@ export const LoadSenseRelation = ({ sense }: { sense: Sense }) => {
 }
 export const LoadSynsetRelation = ({ synsetId }: { synsetId: string }) => {
   // const { senseRelation, synsetRelation } = useSynsetGroupedRelation(sense)
-  const relations = useWordNet<RelationRecord[]>("synsetRelation", [synsetId])
+  const relations = useWordNetQuery<RelationRecord[]>("synsetRelation", [synsetId])
   const rel = relations?.[synsetId]
   if (!rel) {
     return null
