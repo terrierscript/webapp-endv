d2dbae7bc4e8b746e886af7f516fc2e024b0404e
terrierscript <13282103+terrierscript@users.noreply.github.com>
2021-04-14 22:43:30 +0900

    add

--- src/pages/api/quiz/question/[word].ts
+++ src/pages/api/quiz/question/[word].ts
@@ -3,22 +3,30 @@ import { RelationRecord } from "../../../../lib/dictionary/types"
 import { getNestedLemma, NestedLemmaData } from "../../../../lib/nested/lemma"
 import { getNestedSynset } from "../../../../lib/nested/synset"
 import { isTruthy } from 'typesafe-utils'
-console.log(isTruthy, isTruthy.toString())
+
+const isQuizWord = (f: string) => {
+  if (!f) return false
+  // if (f === word) return false
+  if (f.split(" ").length !== 1) return false
+  if (f.split("-").length !== 1) return false
+  if (/[A-Z]/.test(f)) return false
+  return true
+
+}
+
 const getWordSynonyms = (word: string, rel: NestedLemmaData) => {
-  console.log("isTruthy", isTruthy)
   const _currentLemmas: string[] = rel.lexicalEntry?.map(l => l.senses.map(se => {
     return se?.synset?.lemmas
-  })).flat(2).filter(isTruthy) ?? []
+  })).flat(2)
+    .filter(isTruthy) ?? []
   const currentLemmas = [...new Set(_currentLemmas)]
-    .filter(f => !!f)
-    .filter(f => f !== word)
-    .filter((f) => f.split(" ").length == 1) // TODO
-    .filter((f) => f.split("-").length == 1) // TODO
+    .filter(isQuizWord)
   return currentLemmas
 }
 
 
 const unique = (arr: (string | undefined)[]): string[] => [...new Set(arr.filter(isTruthy))]
+
 
 const filterParentAndChildren = (rels: RelationRecord[]) => {
   let parents: string[] = []
@@ -32,12 +40,12 @@ const filterParentAndChildren = (rels: RelationRecord[]) => {
     }
   })
   return {
-    parents: [...new Set(parents)],
-    children: [...new Set(children)]
+    parents: [...new Set(parents)].filter(isQuizWord),
+    children: [...new Set(children)].filter(isQuizWord)
   }
 }
 
-const dig = (rel: NestedLemmaData) => {
+const digSynset = (rel: NestedLemmaData) => {
   const rels = rel.lexicalEntry?.map(l => l.senses.map(se => {
     return se?.synset?.relations
   })).flat(2).filter(rel => !!rel)
@@ -53,23 +61,80 @@ const dig = (rel: NestedLemmaData) => {
   const pc = filterParentAndChildren(rels)
   return pc
 }
-const relatedWord = (word: string) => {
+
+const levelIntersect = (targets: string[][]) => {
+  const dups = new Set()
+  const results: string[][] = []
+  targets.forEach(t => {
+    const f = t.filter(item => !dups.has(item))
+    results.push(f)
+    t.forEach(tt => dups.add(tt))
+  })
+  return results
+}
+
+
+const getRelatedWords = (word: string) => {
   const lemma = getNestedLemma(word)
-  const currentLemmas = getWordSynonyms(word, lemma)
-  const lv1 = dig(lemma)
-  // rel.senses.map(s => {
-  //   s.
-  // })
-  // console.log(word)
+  const synonym = getWordSynonyms(word, lemma)
+  const { parents, children } = digSynset(lemma)
   return {
-    lv0: currentLemmas,
-    lv1,
+    word, synonym, parents, children
   }
+}
+
+const addSet = (set: Set<string>, append: string[]) => {
+  append.map(a => set.add(a))
+  return set
+}
+const relatedWords = (words: string[]) => {
+  const m: { [k in string]: Set<string> } = {
+    word: new Set(),
+    synonym: new Set(),
+    parents: new Set(),
+    children: new Set(),
+  }
+  words.map(w => {
+    return getRelatedWords(w)
+  }).forEach(({ word, synonym, parents, children }) => {
+    m.word = addSet(m.word, [word])
+    m.synonym = addSet(m.synonym, synonym)
+    m.parents = addSet(m.parents, parents)
+    m.children = addSet(m.children, children)
+  })
+  return Object.fromEntries(Object.entries(m).map(([k, v]) => {
+    return [k, [...v]]
+  }))
+}
+
+const digCousinWords = (words: string[]) => {
+  const c = relatedWords(words)
+  return c.children
+}
+
+function random(arr: string[]) {
+  return arr[Math.floor(Math.random() * arr.length)]
+}
+
+const relatedWord = (word: string) => {
+  const { synonym, parents } = getRelatedWords(word)
+  // const d1 = relatedWords(parents)
+  // const d2 = relatedWords(d1.parents)
+  const levels = [
+    [word], synonym, parents,
+    // d1.children, d2.children
+  ]
+  return levelIntersect(levels)
 }
 const handler: NextApiHandler = async (req, res) => {
   const { word } = req.query
-  const r = relatedWord(word.toString())
-  res.json(r)
+  const [w, l1, l2] = relatedWord(word.toString())
+
+  res.json({
+    word: w,
+    answer: random(l1),
+    diff: random(l2)
+  })
 }
 
 export default handler
