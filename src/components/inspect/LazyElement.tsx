3552c24b7525aff31f254b3e9d7d0162049dd9c1
terrierscript <13282103+terrierscript@users.noreply.github.com>
2021-04-14 15:24:39 +0900

    add

--- src/components/inspect/LazyElement.tsx
+++ src/components/inspect/LazyElement.tsx
@@ -2,6 +2,7 @@ import { Skeleton, Box, Stack, SkeletonText } from "@chakra-ui/react"
 import React, { FC, useRef } from "react"
 import { useIntersection } from "use-intersection"
 
+
 const LoadingSkelton = () => <Stack>
   <Skeleton height="20px" />
   <Skeleton height="20px" />
@@ -16,10 +17,6 @@ export const LazyElement: FC<{}> = ({ children, }) => {
   })
 
   return <div ref={ref}>
-    <Skeleton isLoaded={intersecting}>
-      {children}
-    </Skeleton>
-
-    {/* {intersecting ? children : <LoadingSkelton />} */}
+    {intersecting ? children : <LoadingSkelton />}
   </div>
 }
