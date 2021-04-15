792f766d2df34d8fd0f811996f9cf6fad8c174c0
terrierscript <13282103+terrierscript@users.noreply.github.com>
2021-04-15 09:43:01 +0900

    404

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -6,22 +6,6 @@ import NextLink from "next/link"
 import nlp from "compromise"
 import { InspectWordLink } from '../components/Link'
 
-const getRandomWord = () => {
-  // @ts-ignore
-  const words = Object.keys(nlp().world?.words)
-  const rand = Math.floor(Math.random() * words.length)
-  return words[rand]
-}
-
-export const getServerSideProps = () => {
-  const randoms = [
-    ...Array.from(Array(10), () => dictionary.getRandomWord()),
-    ...Array.from(Array(10), () => getRandomWord()),
-  ]
-  return {
-    props: { randoms }
-  }
-}
 
 
 // @ts-ignore
@@ -30,11 +14,7 @@ export default function Home({ randoms }) {
     <Stack>
       <Heading>404 Not found</Heading>
       <Wrap>
-        {randoms.map((r: string) => {
-          return <Box key={r}>
-            <InspectWordLink word={r} />
-          </Box>
-        })}
+        <a href="/">Home</a>
       </Wrap>
     </Stack>
   )
