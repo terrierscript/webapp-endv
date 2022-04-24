d7c5273bf6f5fe51e1c23aa88c7d9bc7f1e692af
terrierscript <13282103+terrierscript@users.noreply.github.com>
2022-04-24 15:53:08 +0900

    add

--- packages/quiz-generator/dictionary/dictionary.ts
+++ packages/quiz-generator/dictionary/dictionary.ts
@@ -1,11 +1,11 @@
 import { generateDictionary } from "@terrierscript/normalized-global-wordnet-en"
 
 const dictionary = generateDictionary(
-  {
-    loader: (dirname, dig) => {
-      return require(`../../../node_modules/@terrierscript/normalized-global-wordnet-en/dic/${dirname}/${dig}.json`)
+  // {
+  //   loader: (dirname, dig) => {
+  //     return require(`../../../node_modules/@terrierscript/normalized-global-wordnet-en/dic/${dirname}/${dig}.json`)
 
-    }
-  }
+  //   }
+  // }
 )
 export default dictionary
